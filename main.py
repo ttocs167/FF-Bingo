db1958d7b9356e68ea2072af83cb38a0dac573c1
Scott <s.herod@amrc.co.uk>
2021-04-13 14:12:59 +0100

    Added ability to turn on/off the free space separation
    Added a bool to control free space slot being taken from different pool. If bool is false then all spaces are taken from the master pool
    some new additions to the lists
    Added output folder 
    General code cleanup

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ def draw_text(x_coord, y_coord, text):
              size='xx-large')
 
 
+# number of images to generate and bool to determine if free space is taken from seperate list
 num_images = 8
+free_space_bool = True
+
+list_path = "list.txt"
+free_space_list_path = "free_list.txt"
 
 # specify font used
 font_path = r"C:\Windows\Fonts\arial.ttf"
@@ -29,29 +34,30 @@ blank = np.array(Image.open("blank.png"))
 cell_width = 180
 x_cells = 5
 y_cells = 5
+x_offset = 50  # number of pixels between left edge and start of the bingo grid in the blank
+y_offset = 72  # number of pixels from top of image to top line of the bingo grid in the blank
 
 # calculate the coords of the centre points of each cell
-x_coords = np.linspace(50 + (cell_width//2), (50 + (cell_width//2)) + (cell_width*(x_cells-1)), x_cells)
-y_coords = np.linspace(72 + (cell_width//2), (72 + (cell_width//2)) + (cell_width*(y_cells-1)), y_cells)
+x_coords = np.linspace(x_offset + (cell_width//2), (x_offset + (cell_width//2)) + (cell_width*(x_cells-1)), x_cells)
+y_coords = np.linspace(y_offset + (cell_width//2), (y_offset + (cell_width//2)) + (cell_width*(y_cells-1)), y_cells)
 
 # make a single list of tuples containing all coords
+# this is so the centre square can be popped from a single list
 all_coords = list(itertools.product(x_coords, y_coords))
 
-# decide where you want the free space
-free_space = (2, 2)
-# pop the free space coord out of the list
-free_space_coord = all_coords.pop((free_space[0] * x_cells) + free_space[1])
+if free_space_bool:
+    # decide where you want the free space
+    free_space = (2, 2)
+    # pop the free space coord out of the list
+    free_space_coord = all_coords.pop((free_space[0] * x_cells) + free_space[1])
 
 # loop to generate n random bingo cards
 for image_num in range(num_images):
 
     # lists must be loaded every loop for random.choice to resample ¯\_(ツ)_/¯
-    statements = np.loadtxt("list.txt", dtype=str, comments="#", delimiter="\n", unpack=False)
-    free_statements = np.loadtxt("free_list.txt", dtype=str, comments="#", delimiter="\n", unpack=False)
-
-    # generate the random values for this card
+    # then generate the random values for this card
+    statements = np.loadtxt(list_path, dtype=str, comments="#", delimiter="\n", unpack=False)
     random_choices = np.random.choice(statements, (x_cells * y_cells), replace=False)
-    random_free_space = np.random.choice(free_statements, 1, replace=False)
 
     # create the plt figure to be drawn on and saved later
     fig = plt.figure(figsize=(14, 14))
@@ -67,15 +73,19 @@ for image_num in range(num_images):
                       (-len(wrapped_statements) * line_height / 2) + all_coords[i][1] + k * line_height,
                       wrapped_statements[k])
 
-    # write the free space
-    wrapped_free_statements = text_wrap(random_free_space[0], font, cell_width * 0.9)
-    for k in range(len(wrapped_free_statements)):
-        draw_text(free_space_coord[0],
-                  (-len(wrapped_free_statements) * line_height / 2) + free_space_coord[1] + k * line_height,
-                  wrapped_free_statements[k])
+    # handle free space text
+    if free_space_bool:
+        free_statements = np.loadtxt(free_space_list_path, dtype=str, comments="#", delimiter="\n", unpack=False)
+        random_free_space = np.random.choice(free_statements, 1, replace=False)
+        # write the free space
+        wrapped_free_statements = text_wrap(random_free_space[0], font, cell_width * 0.9)
+        for k in range(len(wrapped_free_statements)):
+            draw_text(free_space_coord[0],
+                      (-len(wrapped_free_statements) * line_height / 2) + free_space_coord[1] + k * line_height,
+                      wrapped_free_statements[k])
 
-    ax.imshow(blank)
+    ax.imshow(blank)  # must imshow or figure cannot be saved
     # finally save the output images
-    plt.savefig('output_' + str(image_num) + '_.png', bbox_inches='tight', pad_inches=0)
+    plt.savefig('output_folder/output_' + str(image_num) + '_.png', bbox_inches='tight', pad_inches=0)
 
 

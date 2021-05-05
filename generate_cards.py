81e4e1f8f4b3fae3e6a2e540a00377d876d6fcb6
Scott <s.herod@amrc.co.uk>
2021-05-05 13:43:47 +0100

    Added ability to change the lists via bot command
    added $add $del $list $RESETLIST commands to the bot as well as their free space variants
    missing a reset free list command for now.

--- generate_cards.py
+++ generate_cards.py
@@ -14,9 +14,8 @@ def draw_text(x_coord, y_coord, text):
              size='xx-large')
 
 
-def generate_card(image_index):
+def generate_card(image_index, num_images=1):
     # number of images to generate and bool to determine if free space is taken from seperate list
-    num_images = 1
     free_space_bool = True
 
     list_path = "list.txt"
@@ -24,11 +23,10 @@ def generate_card(image_index):
 
     # specify font used
     font_path = r"Montserrat-Regular.ttf"
-    font = ImageFont.truetype(font=font_path, size=30)
+    font = ImageFont.truetype(font=font_path, size=28)
     # this value determines how the lines are spaced vertically when text is wrapped
     line_height = 22
-    # names on the cards
-    names = ["Me", "Noah", "Ziggy", "Twink", "Jabber", "Hux", "Fedi", "Robin"]
+
     # load in the blank bingo card image
     blank = np.array(Image.open("blank.png"))
 
@@ -54,42 +52,39 @@ def generate_card(image_index):
         free_space_coord = all_coords.pop((free_space[0] * x_cells) + free_space[1])
 
     # loop to generate n random bingo cards
+    for image_num in range(num_images):
+        # lists must be loaded every loop for random.choice to resample ¯\_(ツ)_/¯
+        # then generate the random values for this card
+        statements = np.loadtxt(list_path, dtype=str, comments="#", delimiter="\n", unpack=False)
+        random_choices = np.random.choice(statements, (x_cells * y_cells), replace=False)
 
-    # lists must be loaded every loop for random.choice to resample ¯\_(ツ)_/¯
-    # then generate the random values for this card
-    statements = np.loadtxt(list_path, dtype=str, comments="#", delimiter="\n", unpack=False)
-    random_choices = np.random.choice(statements, (x_cells * y_cells), replace=False)
+        # create the plt figure to be drawn on and saved later
+        fig = plt.figure(figsize=(14, 14))
+        ax = plt.axes(frameon=False, xticks=[], yticks=[])
 
-    # create the plt figure to be drawn on and saved later
-    fig = plt.figure(figsize=(14, 14))
-    ax = plt.axes(frameon=False, xticks=[], yticks=[])
+        for i in range(len(all_coords)):
 
-    for i in range(len(all_coords)):
+            # calculate text wrapping (split into lines with text_wrap() func)
+            wrapped_statements = text_wrap(random_choices[i], font, cell_width * 0.9)
+            # loop through the splits and place the lines with plt.text()
+            for k in range(len(wrapped_statements)):
+                draw_text(all_coords[i][0],
+                          (-len(wrapped_statements) * line_height / 2) + all_coords[i][1] + k * line_height,
+                          wrapped_statements[k])
 
-        # calculate text wrapping (split into lines with text_wrap() func)
-        wrapped_statements = text_wrap(random_choices[i], font, cell_width * 0.9)
-        # loop through the splits and place the lines with plt.text()
-        for k in range(len(wrapped_statements)):
-            draw_text(all_coords[i][0],
-                    (-len(wrapped_statements) * line_height / 2) + all_coords[i][1] + k * line_height,
-                    wrapped_statements[k])
+        # handle free space text
+        if free_space_bool:
+            free_statements = np.loadtxt(free_space_list_path, dtype=str, comments="#", delimiter="\n", unpack=False)
+            random_free_space = np.random.choice(free_statements, 1, replace=False)
+            # write the free space
+            wrapped_free_statements = text_wrap(random_free_space[0], font, cell_width * 0.9)
+            for k in range(len(wrapped_free_statements)):
+                draw_text(free_space_coord[0],
+                          (-len(wrapped_free_statements) * line_height / 2) + free_space_coord[1] + k * line_height,
+                          wrapped_free_statements[k])
 
-    # handle free space text
-    if free_space_bool:
-        free_statements = np.loadtxt(free_space_list_path, dtype=str, comments="#", delimiter="\n", unpack=False)
-        random_free_space = np.random.choice(free_statements, 1, replace=False)
-        # write the free space
-        wrapped_free_statements = text_wrap(random_free_space[0], font, cell_width * 0.9)
-        for k in range(len(wrapped_free_statements)):
-            draw_text(free_space_coord[0],
-                    (-len(wrapped_free_statements) * line_height / 2) + free_space_coord[1] + k * line_height,
-                    wrapped_free_statements[k])
-
-    if len(names) == num_images:
-        # write names on images
-        draw_text(900, 988, names[image_index])
-
-    ax.imshow(blank)  # must imshow or figure cannot be saved
-    # finally save the output images
-    fig.savefig('output_folder/output_' + str(image_index) + '.png', bbox_inches='tight', pad_inches=0)
+        ax.imshow(blank)  # must imshow or figure cannot be saved
+        # finally save the output images
+        fig.savefig('output_folder/output_' + str(image_index) + '.png', bbox_inches='tight', pad_inches=0)
+        image_index = (image_index + 1) % 5
 
--- main.py
+++ main.py
@@ -36,18 +36,69 @@ async def on_message(message):
 
             print('image generated!')
 
-            rolling_index = (rolling_index + 1) % 20
+            rolling_index = (rolling_index + 1) % 5
             time_of_last_bingo = time.time()
 
         else:
             print("bingo command recieved, too soon to generate!")
 
+    if message.content.startswith('$add'):
+        line = message.content.split("$add ", 1)[1]
+        await message.channel.send("New line: \n_" + line + "_ \nAdded to pool!")
+        await utils.add_to_list(line)
+        print("New line: _" + line + "_ Added to pool!")
+
+    if message.content.startswith('$addfree'):
+        line = message.content.split("$addfree ", 1)[1]
+        await message.channel.send("New line: \n_" + line + "_ \nAdded to free space pool!")
+        await utils.add_to_free_list(line)
+        print("New line: _" + line + "_ Added to free space pool!")
+
+    if message.content.startswith('$refresh'):
+        # regenerate all images
+        await regenerate_all_images()
+        await message.channel.send("Cards refreshed!")
+
     if message.content.startswith('$animal'):
         await message.channel.send(utils.random_animal_emoji())
 
+    if message.content.startswith('$list'):
+        lines = await utils.list_all_lines()
+        await message.channel.send(lines)
 
+    if message.content.startswith('$listfree'):
+        lines = await utils.list_all_free_lines()
+        await message.channel.send(lines)
+
+    if message.content.startswith('$del'):
+        argument = message.content.split("$del ", 1)[1]
+        try:
+            index = int(argument)
+            await utils.delete_line(index)
+            await message.channel.send("deleted line: " + str(index))
+        except:
+            await message.channel.send(argument + " is not an integer you dingus!")
+
+    if message.content.startswith('$delfree'):
+        argument = message.content.split("$delfree ", 1)[1]
+        try:
+            index = int(argument)
+            await utils.delete_free_line(index)
+            await message.channel.send("deleted free space line: " + str(index))
+        except:
+            await message.channel.send(argument + " is not an integer you dingus!")
+
+    if message.content.startswith('$RESETLIST'):
+        await utils.reset_list()
+        await message.channel.send("List has been reset to default.")
+
+        
 async def regenerate_images(index):
     generate_card(index)
 
+
+async def regenerate_all_images():
+    generate_card(0, 5)
+    print("ALL CARDS REGENERATED")
 
 client.run(os.getenv('BINGO_BOT_TOKEN'))

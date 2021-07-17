b32c9a0eb49565009a329a376a48ca06c425a071
Scott <s.herod@amrc.co.uk>
2021-07-17 22:21:04 +0100

    Updated AI command to be a simple QA bot

--- utilities/openAI_test.py
+++ utilities/openAI_test.py
@@ -8,13 +8,34 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-start_sequence = "\nBingoBot:"
-restart_sequence = "\nHuman: "
+start_sequence = "\nA:"
+restart_sequence = "\n\nQ: "
 
-prompt_header = "The following is a conversation with an AI assistant called BingoBot and a chat room of friends. " \
-                "BingoBot is helpful, creative, clever, kind, supporting and very friendly." \
-                "\n\nHuman: Hello, who are you?" \
-                "\nBingoBot: I am BingoBot, an AI assistant here to help and entertain you!"
+prompt_header = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth," \
+                " I will give you the answer. If you ask me a question that is nonsense," \
+                " trickery, or has no clear answer, I will respond with \"Unknown\".\n\n" \
+                "Q: What is human life expectancy in the United States?\n" \
+                "A: Human life expectancy in the United States is 78 years.\n\n" \
+                "Q: Who was president of the United States in 1955?\n" \
+                "A: Dwight D. Eisenhower was president of the United States in 1955.\n\n" \
+                "Q: Which party did he belong to?\n" \
+                "A: He belonged to the Republican Party.\n\n" \
+                "Q: What is the square root of banana?\n" \
+                "A: Unknown\n\n" \
+                "Q: How does a telescope work?\n" \
+                "A: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\n" \
+                "Q: Where were the 1992 Olympics held?\n" \
+                "A: The 1992 Olympics were held in Barcelona, Spain.\n\n" \
+                "Q: How many squigs are in a bonk?\n" \
+                "A: Unknown"
+
+# start_sequence = "\nBingoBot:"
+# restart_sequence = "\nHuman: "
+
+# prompt_header = "The following is a conversation with an AI assistant called BingoBot and a chat room of friends. " \
+#                 "BingoBot is helpful, creative, clever, kind, supporting and very friendly." \
+#                 "\n\nHuman: Hello, who are you?" \
+#                 "\nBingoBot: I am BingoBot, an AI assistant here to help and entertain you!"
 
 recent_history = deque([], maxlen=10)
 
@@ -35,17 +56,17 @@ recent_history = deque([], maxlen=10)
 def get_ai_response(new_text, author='Human'):
     global recent_history
 
-    recent_history.append("\n" + author + ": " + new_text + "\nBingoBot: ")
+    recent_history.append("\n\nQ: " + new_text + "\nA:")
 
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt_header + ''.join(recent_history),
-        temperature=0.9,
-        max_tokens=64,
+        temperature=0,
+        max_tokens=100,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", "Human:", "BingoBot"]
+        presence_penalty=0,
+        stop=["\n"]
     )
 
     response = json.loads(json.dumps(response))
@@ -54,6 +75,6 @@ def get_ai_response(new_text, author='Human'):
 
     recent_history.append(response_text)
 
-    print(recent_history)
+    print(''.join(recent_history))
 
     return response_text

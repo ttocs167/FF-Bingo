4095d64e6aa2c24bbfe91fb280303cbaa52d5936
Scott <s.herod@amrc.co.uk>
2021-10-30 22:44:28 +0100

    added new command

--- main.py
+++ main.py
@@ -185,6 +185,15 @@ class Bot(commands.Bot):
 
         await ctx.reply(out)
 
+    @commands.command()
+    async def ffxiv(ctx):
+        pasta = "Did you know that the critically acclaimed MMORPG Final Fantasy XIV has a free trial," \
+                " and includes the entirety of A Realm Reborn AND the award-winning Heavensward expansion up to" \
+                " level 60 with no restrictions on playtime? Sign up, and enjoy Eorzea today!" \
+                " https://secure.square-enix.com/account/app/svc/ffxivregister?lng=en-gb"
+
+        await ctx.send(pasta)
+
     @commands.command(name='rig', hidden=True)
     @commands.is_owner()
     async def rig(ctx, *, line):

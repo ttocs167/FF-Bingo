30e6af4dfc8f02779b0e650c5a4fa546d4289b93
Scott <s.herod@amrc.co.uk>
2022-05-07 17:25:16 +0100

    changes to camera settings

--- utilities/webcam_photo.py
+++ utilities/webcam_photo.py
@@ -1,11 +1,24 @@
 import cv2
 from datetime import datetime
+import subprocess
+
+cam_props = {'brightness': 128, 'contrast': 128, 'saturation': 180,
+             'gain': 0, 'sharpness': 128, 'exposure_auto': 1,
+             'exposure_absolute': 150, 'exposure_auto_priority': 0,
+             'focus_auto': 0, 'focus_absolute': 30, 'zoom_absolute': 250,
+             'white_balance_temperature_auto': 0, 'white_balance_temperature': 3300}
+
+for key in cam_props:
+    subprocess.call(['v4l2-ctl -d /dev/video1 -c {}={}'.format(key, str(cam_props[key]))],
+                    shell=True)
+
+print("initialising webcam...")
+cam = cv2.VideoCapture(0)
 
 
 async def take_image():
 
-    print("initialising webcam...")
-    cam = cv2.VideoCapture(0)
+    # cam.set(cv2.CAP_PROP_EXPOSURE, 1)
 
     timestamp = datetime.now().strftime("%m_%d_%Y, %H-%M-%S")
     filename = "resources/plant_images/" + timestamp + ".png"

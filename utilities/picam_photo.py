b5770205676d8d1dd1bd132a3394e0658f703b86
Scott <s.herod@amrc.co.uk>
2022-05-21 18:58:45 +0100

    picam changes

--- utilities/picam_photo.py
+++ utilities/picam_photo.py
@@ -2,21 +2,25 @@ from datetime import datetime
 import PiCamera
 from time import sleep
 
-print("initialising Pi Camera...")
+print("Initialising PiCam...")
 camera = PiCamera()
-camera.resolution = (1024, 768)
+camera.resolution = (1640, 1232)
+camera.framerate_range = (0.16666, 60)
 camera.start_preview()
+camera.annotate_background = PiCamera.Color('black')
 
 # Camera warm-up time
 sleep(2)
 # await asyncio.sleep(2)
+
+print("PiCam ready!")
 
 
 async def take_image():
 
     timestamp = datetime.now().strftime("%m_%d_%Y, %H-%M-%S")
     filename = "resources/plant_images/" + timestamp + ".png"
-
+    camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     camera.capture(filename)
 
     return filename

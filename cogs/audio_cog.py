bfc52935aa7d4ab3878dc663f8d61a38734c9218
Scott <s.herod@amrc.co.uk>
2022-09-19 23:22:12 +0100

    added url audio test

--- cogs/audio_cog.py
+++ cogs/audio_cog.py
@@ -2,6 +2,9 @@ import discord
 from discord.ext import commands
 import time
 from os.path import exists
+import urllib
+import re
+import pafy
 
 
 class AudioCog(commands.Cog):
@@ -25,6 +28,7 @@ class AudioCog(commands.Cog):
             except TimeoutError:
                 await ctx.reply("connection to voice channel timed out")
                 return
+            # vc.source = discord.PCMVolumeTransformer(vc.source, volume=0.5)
             print("playing voice clip: " + args)
             vc.play(discord.FFmpegPCMAudio('resources/audio_clips/' + args + '.mp3', options='-filter:a "volume=0.3"'),
                     after=lambda e: print('done', e))
@@ -33,3 +37,40 @@ class AudioCog(commands.Cog):
             await vc.disconnect()
         else:
             await ctx.send(str(ctx.author.name) + "is not in a channel.")
+
+    @commands.command(name='soundurl')
+    async def play_sound_from_url(self, ctx: commands.Context):
+
+        search = "morpheus tutorials discord bot python"
+        ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                          'options': '-vn'}
+
+        if ctx.message.author.voice is None:
+            await ctx.send("You need to be in a voice channel to use this command!")
+            return
+
+        voice_channel = ctx.author.voice.channel
+        if voice_channel is not None:
+            try:
+                vc = await voice_channel.connect()
+            except TimeoutError:
+                await ctx.reply("connection to voice channel timed out")
+                return
+
+        search = search.replace(" ", "+")
+
+        html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search)
+        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+
+        await ctx.send("https://www.youtube.com/watch?v=" + video_ids[0])
+
+        song = pafy.new(video_ids[0])  # creates a new pafy object
+
+        audio = song.getbestaudio()  # gets an audio source
+
+        source = discord.FFmpegPCMAudio(audio.url,
+                                        **ffmpeg_options)  # converts the youtube audio source into a source discord can use
+
+        vc.play(source)  # play the source
+
+

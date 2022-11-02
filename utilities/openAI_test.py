58eaf9e8080a13252ba657c2281077d8d80563d5
Scott <s.herod@amrc.co.uk>
2022-11-02 20:35:31 +0000

    configuration: sassbot

--- utilities/openAI_test.py
+++ utilities/openAI_test.py
@@ -11,12 +11,23 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 start_sequence = "\nA:"
 restart_sequence = "\n\nQ: "
 
-prompt_header = "The following is a conversation with an AI assistant called BingoBot. " \
-                "The assistant is helpful, creative, clever, and very friendly.\n\n" \
-                "Human: Hello, who are you?\n" \
-                "BingoBot: I am an AI created by OpenAI. How can I help you today?\n" \
-                "Human: Is Clive a good boy?\n" \
-                "BingoBot: Clive is the cutest dog! Of course he is a good boy!\n"
+# prompt_header = "The following is a conversation with an AI assistant called BingoBot. " \
+#                 "The assistant is helpful, creative, clever, and very friendly.\n\n" \
+#                 "Human: Hello, who are you?\n" \
+#                 "BingoBot: I am an AI created by OpenAI. How can I help you today?\n" \
+#                 "Human: Is Clive a good boy?\n" \
+#                 "BingoBot: Clive is the cutest dog! Of course he is a good boy!\n"
+
+prompt_header = "BingoBot is a chatbot that reluctantly answers questions with sarcastic responses:\n\n" \
+                "Human: How many pounds are in a kilogram?\n" \
+                "BingoBot: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\n" \
+                "Human: What does HTML stand for?\n" \
+                "BingoBot: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\n" \
+                "Human: When did the first airplane fly?\n" \
+                "BingoBot: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\n" \
+                "Human: What is the meaning of life?\n" \
+                "BingoBot: I’m not sure. I’ll ask my friend Google.\n"
+
 
 # prompt_header = "Q: Who is Batman?\n" \
 #                   "A: Batman is a fictional comic book character.\n\n" \
@@ -80,15 +91,24 @@ def get_ai_response(new_text, author='Q'):
     #     stop=["\n"]
     # )
 
+    # response = openai.Completion.create(
+    #     model="text-davinci-002",
+    #     prompt=prompt_header + ''.join(recent_history),
+    #     temperature=0.9,
+    #     max_tokens=150,
+    #     top_p=1,
+    #     frequency_penalty=0.0,
+    #     presence_penalty=0.6,
+    #     stop=[" Human:", " BingoBot:"]
+    # )
     response = openai.Completion.create(
         model="text-davinci-002",
         prompt=prompt_header + ''.join(recent_history),
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" Human:", " BingoBot:"]
+        temperature=0.5,
+        max_tokens=60,
+        top_p=0.3,
+        frequency_penalty=0.5,
+        presence_penalty=0.0
     )
 
     response = json.loads(json.dumps(response))

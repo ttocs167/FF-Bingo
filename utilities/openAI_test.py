4f9734a1f7c59f5c795df55b889f0f0895a8fd4f
Scott <s.herod@amrc.co.uk>
2022-11-02 19:13:47 +0000

    Feat: reviving AI bot!

--- utilities/openAI_test.py
+++ utilities/openAI_test.py
@@ -11,23 +11,44 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 start_sequence = "\nA:"
 restart_sequence = "\n\nQ: "
 
-prompt_header = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth," \
-                " I will give you the answer. If you ask me a question that is nonsense," \
-                " trickery, or has no clear answer, I will respond with \"Unknown\".\n\n" \
-                "Q: What is human life expectancy in the United States?\n" \
-                "A: Human life expectancy in the United States is 78 years.\n\n" \
-                "Q: Who was president of the United States in 1955?\n" \
-                "A: Dwight D. Eisenhower was president of the United States in 1955.\n\n" \
-                "Q: Which party did he belong to?\n" \
-                "A: He belonged to the Republican Party.\n\n" \
-                "Q: What is the square root of banana?\n" \
-                "A: Unknown\n\n" \
-                "Q: How does a telescope work?\n" \
-                "A: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\n" \
-                "Q: Where were the 1992 Olympics held?\n" \
-                "A: The 1992 Olympics were held in Barcelona, Spain.\n\n" \
-                "Q: How many squigs are in a bonk?\n" \
-                "A: Unknown"
+# prompt_header = "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth," \
+#                 " I will give you the answer. If you ask me a question that is nonsense," \
+#                 " trickery, or has no clear answer, I will respond with \"Unknown\".\n\n" \
+#                 "Q: What is human life expectancy in the United States?\n" \
+#                 "A: Human life expectancy in the United States is 78 years.\n\n" \
+#                 "Q: Who was president of the United States in 1955?\n" \
+#                 "A: Dwight D. Eisenhower was president of the United States in 1955.\n\n" \
+#                 "Q: Which party did he belong to?\n" \
+#                 "A: He belonged to the Republican Party.\n\n" \
+#                 "Q: What is the square root of banana?\n" \
+#                 "A: Unknown\n\n" \
+#                 "Q: How does a telescope work?\n" \
+#                 "A: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\n" \
+#                 "Q: Where were the 1992 Olympics held?\n" \
+#                 "A: The 1992 Olympics were held in Barcelona, Spain.\n\n" \
+#                 "Q: How many squigs are in a bonk?\n" \
+#                 "A: Unknown\n\n"
+prompt_header = "Q: Who is Batman?\n" \
+                  "A: Batman is a fictional comic book character.\n\n" \
+                  "Q: What is torsalplexity?\n" \
+                  "A: ?\n\n" \
+                  "Q: What is Devz9?\n" \
+                  "A: ?\n\n" \
+                  "Q: Who is George Lucas?\n" \
+                  "A: George Lucas is American film director and producer famous for creating Star Wars.\n\n" \
+                  "Q: What is the capital of California?\n" \
+                  "A: Sacramento.\n\n" \
+                  "Q: What orbits the Earth?\n" \
+                  "A: The Moon.\n\n" \
+                  "Q: Who is Fred Rickerson?\n" \
+                  "A: ?\n\nQ: What is an atom?\n" \
+                  "A: An atom is a tiny particle that makes up everything.\n\n" \
+                  "Q: Who is Alvan Muntz?\n" \
+                  "A: ?\n\n" \
+                  "Q: What is Kozar-09?\n" \
+                  "A: ?\n\n" \
+                  "Q: How many moons does Mars have?\n" \
+                  "A: Two, Phobos and Deimos.\n\n"
 
 # start_sequence = "\nBingoBot:"
 # restart_sequence = "\nHuman: "
@@ -53,13 +74,13 @@ recent_history = deque([], maxlen=10)
 # response = json.loads(json.dumps(response))
 
 
-def get_ai_response(new_text, author='Human'):
+def get_ai_response(new_text, author='Q'):
     global recent_history
 
     recent_history.append("\n\nQ: " + new_text + "\nA:")
 
     response = openai.Completion.create(
-        engine="davinci",
+        engine="text-davinci-002",
         prompt=prompt_header + ''.join(recent_history),
         temperature=0,
         max_tokens=100,

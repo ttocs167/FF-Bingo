f537e679a467f5f83b3be1c60de3acb30229baca
Scott <s.herod@amrc.co.uk>
2023-05-06 16:40:59 +0100

    enabled camera command for plant images again

--- cogs/camera_cog.py
+++ cogs/camera_cog.py
@@ -14,8 +14,8 @@ class CamCog(commands.Cog):
     @commands.command(name="plant", hidden=False)
     async def webcam_image(self, ctx):
         async with ctx.channel.typing():
-            await ctx.reply("Sorry, the plants are away for winter! But don't worry, we'll be back next year"
-                            " :slight_smile:")
-        #     image_path = await picam_photo.take_image()
-        #     img = discord.File(image_path)
-        # await ctx.reply("", file=img)
+            # await ctx.reply("Sorry, the plants are away for winter! But don't worry, we'll be back next year"
+            #                 " :slight_smile:")
+            image_path = await picam_photo.take_image()
+            img = discord.File(image_path)
+        await ctx.reply("", file=img)
